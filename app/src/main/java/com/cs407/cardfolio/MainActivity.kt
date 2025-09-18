d6c3e7c64f63440a7b9e9759c86af93fed7d79a2
sadasiebert <smsiebert@wisc.edu>
2025-09-18 17:13:50 -0500

    Page 10/26

--- app/src/main/java/com/cs407/cardfolio/MainActivity.kt
+++ app/src/main/java/com/cs407/cardfolio/MainActivity.kt
@@ -1,12 +1,39 @@
 package com.cs407.cardfolio
 
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -17,6 +44,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.cs407.cardfolio.ui.theme.CardfolioTheme
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.layout.Box
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,8 +71,38 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Cardfolio () {
+fun Cardfolio() {
 
+    var name by remember { mutableStateOf("") }
+    var name: String = ""
+    var hobby: String = ""
+
+    Box(
+        contentAlignment = Alignment.Center,
+    ) {
+        ElevatedCard (
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight(),
+        ) {
+            Row (
+
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.download),
+                    contentDescription = "Profile Image",
+                )
+                Column {
+                    Text(
+                        text = if(name.isBlank()) stringResource(id = R.string.card_name) else name,
+                    )
+                    Text(
+                        text = if(hobby.isBlank()) stringResource(id = R.string.card_hobby) else hobby,
+                    )
+                }
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
