60615b663dfca881ba7a00f188c4d16e04a12339
sadasiebert <smsiebert@wisc.edu>
2025-09-18 14:39:18 -0500

    Initial Commit

--- app/src/main/java/com/cs407/cardfolio/MainActivity.kt
+++ app/src/main/java/com/cs407/cardfolio/MainActivity.kt
@@ -4,14 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Preview
 import com.cs407.cardfolio.ui.theme.CardfolioTheme
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.stringResource
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,11 +24,16 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CardfolioTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Column (
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Text (
+                        text = stringResource(id = R.string.app_name),
+                        style = MaterialTheme.typography.headlineLarge,
+                        modifier = Modifier.padding(top = 16.dp, bottom = 24.dp)
                     )
+                    Cardfolio()
                 }
             }
         }
@@ -31,17 +41,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Cardfolio () {
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     CardfolioTheme {
-        Greeting("Android")
+        Cardfolio()
     }
 }
